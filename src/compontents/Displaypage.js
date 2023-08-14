38359ce1a65ed973a2fee037bec883236473b828
ss5815190 <ss5815190@email.com>
2023-08-14 14:16:30 +0800

    ToTop

--- src/compontents/Displaypage.js
+++ src/compontents/Displaypage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ApiContext } from './GetApi'
 
 const Card=(e)=>{
@@ -28,13 +28,39 @@ const Card=(e)=>{
 }
 const Displaypage = () => {
     const{district}=useContext(ApiContext)
+    useEffect(()=>{
+      const btn=document.getElementById("totop")
+      const top=()=>{
+        if(window.scrollY!==0){
+          btn.classList.add('active');
+        }
+        else{
+          btn.classList.remove('active');
+        }
+      }
+      top()
+      window.addEventListener('scroll', top, true);
+      return () => {
+        window.removeEventListener('scroll', top, true);
+     };
+    }
+    ,[])
   return (
     <main className='content'>
+    <h2 style={{ display: district.length>0 ? 'block' : 'none' }}>
+    {district.length>0 ? district[0].Add.slice(6,9) : ''}
+    </h2>
     {district.map((e,id)=>(
         <Card key={id} img={e.Picture1} opentime={e.Opentime} 
          phone={e.Tel} address={e.Add} name={e.Name} 
          area={e.Add.slice(6,9)}/>
     ))}
+    <button className="totop" id='totop'>
+    <img onClick={() =>{
+      window.scrollTo(0, 0)
+      }
+    } src="./images/icon_goTop.png" alt="" />
+    </button>
     </main>
   )
   
